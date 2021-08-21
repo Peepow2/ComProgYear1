60f0289c7f9bb263b15756f682739d6d2ae7998b
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2021-08-22 00:00:24 +0700

    Update 02_StrList_08 (Decimal2Fraction).py

--- String List/02_StrList_08 (Decimal2Fraction)/02_StrList_08 (Decimal2Fraction).py
+++ String List/02_StrList_08 (Decimal2Fraction)/02_StrList_08 (Decimal2Fraction).py
@@ -1,1 +1,6 @@
-
+""" Python Language
+ # Task: 02_StrList_08 (Decimal2Fraction)
+ # Code: Peerawich Sodsuay
+ # Warning: Don't copy this code to submit.
+ # If you do it, your score will be cancel immediately.
+"""
