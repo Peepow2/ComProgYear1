12f0c6cc5dc5c06559abe0f42b86b1a8a743393f
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2021-08-25 18:08:26 +0700

    Update 02_StrList_08 (Decimal2Fraction).py

--- String List/02_StrList_08 (Decimal2Fraction)/02_StrList_08 (Decimal2Fraction).py
+++ String List/02_StrList_08 (Decimal2Fraction)/02_StrList_08 (Decimal2Fraction).py
@@ -4,3 +4,16 @@
  # Warning: Don't copy this code to submit.
  # If you do it, your score will be cancel immediately.
 """
+import math as m
+str1 = input().strip()
+str1 = str1.split(",")
+a = str(str1[0]); b = str(str1[1]); c = str(str1[2]);
+leb = len(b); lec = len(c);
+num1 = int(a + b);
+num2 = int(a + b + c);
+fac = num2 - num1;
+dev = 10**(leb + lec) - 10**(leb)
+GCD = m.gcd(fac, dev)
+fac //= GCD; dev //= GCD; 
+print(fac,"/",dev)
+
