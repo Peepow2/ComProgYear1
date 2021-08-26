aa88d1bdbd8b9988b65fb34e34dfa5c81597da81
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2021-08-27 00:26:51 +0700

    Add files via upload

---
+++ Homework (1-2564)/HW2_6431130621.py
@@ -0,0 +1,9 @@
+import math as m
+def f(x, a):
+  # y = a * ( b * sqrt(c * (x + d) - e) + f)
+  # a = 1000, b = -2, c = 2.7, d= 14.3, e = 2.2, f = 34.4
+  y = 1000 * (-2 * (m.sqrt(2.7 * (x + 14.3) - 2.2)) + 34.4);
+  return y;
+
+days = 3;
+student_name= "นายพีรวิชญ์ สดสวย" 
