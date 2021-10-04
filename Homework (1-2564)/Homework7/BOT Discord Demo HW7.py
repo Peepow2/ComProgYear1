588493a554f9f38ed2b70c8be7fb5225e601ec6c
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2021-10-04 23:21:46 +0700

    Update BOT Discord Demo HW7.py

--- Homework (1-2564)/Homework7/BOT Discord Demo HW7.py
+++ Homework (1-2564)/Homework7/BOT Discord Demo HW7.py
@@ -34,12 +34,7 @@ async def on_message(message):
             question += " -->"
         await message.author.send(question)
 
-# HW7: Discord Examiner bot
-# พิมพ์ เลขประจำตัว ชื่อ และนามสกุลของนิสิต
-# 6431130621 พีรวิชญ์ สดสวย
 def sign_in(uid, sid, exam_info):
-    # เขียนโปรแกรมในส่วนนี้
-    # คืนผลเป็นข้อมูลการสอบของนิสิตที่มี uid ตรงกับ uid ที่ function รับเข้ามา
     if len(exam_info) == 0:
         exam_info += [[uid, sid, [], [], 0]]
     ids = []; idu = [];
@@ -52,10 +47,6 @@ def sign_in(uid, sid, exam_info):
     return exam_info
 
 def get_welcome_and_rules_msg():
-    # เขียนโปรแกรมในส่วนนี้
-    # คืนผลเป็นข้อความต้อนรับและกฎกติกาในการสอบ โดยให้นิสิตสมมุติว่าถ้าตัวเองเป็นกรรมการคุมสอบ 
-    # จะมีการกล่าวต้อนรับอย่างไร และมีกฎกติกาอะไรต้องแจ้งบ้าง ออกทางหน้าจอ
-
     Rules = str("")
     # Rules += "พ่อมึงตาย ISUS มึงเข้ามาสอบทำไมวะ เขียนชื่อแล้วก็ออกไป คะแนนก็ได้เท่ากับตอนที่มึงสอบจริงนั่นแหละนะ"
     Rules += "สวัสดีครับนักศึกษาทุกคน วันนี้เป็นวันสอบวิชาการเขียนโปรแกรมนะครับ" + "\n"
@@ -67,18 +58,12 @@ def get_welcome_and_rules_msg():
     return Rules     
 
 def get_student_info(uid, exam_info):
-    # เขียนโปรแกรมในส่วนนี้
-    # คืนผลเป็นข้อมูลการสอบของนิสิตที่มี uid ตรงกับ uid ที่ function รับเข้ามา
     for temp in exam_info:
       if uid == temp[0]:
         return temp
     return None
 
 def get_question(uid, exam_info, all_questions):
-    # คืนผลเป็นคำถามจาก questions ที่ถูกชี้ด้วย qidx ถ้า ณ ตำแหน่งนั้นยังไม่มีคำถาม function นี้
-    # จะต้องไปสุ่มเลือกคำถามมาจาก all_questions ซึ่งจะต้องเป็นคำถามที่นิสิตยังไม่ได้ตอบ
-    # นอกจากจะคืนผลแล้ว function นี้จะต้องเพิ่มคำถามที่ได้ ต่อท้าย questions ด้วย
-    # แต่หากว่าคำถามหมดแล้วก็จะคืนคำว่า end มาให้โดยไม่เปลี่ยนแปลงใดๆกับ questions
     idx = int(random.random() * len(all_questions) // 1)
     temp = get_student_info(uid, exam_info)
     while all_questions[idx] in temp[2]:
@@ -90,8 +75,6 @@ def get_question(uid, exam_info, all_questions):
     return all_questions[idx]
 
 def submit_answer(uid, answer, exam_info):
-    # แก้ไขค่าที่ qidx ชี้อยู่ที่ answers ให้มีค่าเป็น answer
-    # แต่ถ้าตำแหน่ง qidx ยังไม่มีค่าก็จะเอา answer ไปต่อท้าย ถัดจากนั้นก็จะเพิ่มค่า qidx ให้ชี้ไปยังคำถามถัดไป
     info = get_student_info(uid, exam_info)
     if int(info[4]) == len(info[3]):
         info[3] += [answer]
