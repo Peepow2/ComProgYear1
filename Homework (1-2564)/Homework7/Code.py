7d66e18bc2c771fa8ebe75a9dcf00a3675894556
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2021-10-04 23:02:29 +0700

    Update Code.py

--- Homework (1-2564)/Homework7/Code.py
+++ Homework (1-2564)/Homework7/Code.py
@@ -9,8 +9,8 @@ def sign_in(uid, sid, exam_info):
     ids = []; idu = [];
     size = len(exam_info)
     for i in range(size):
-        idu += [exam_info[0]]
-        ids += [exam_info[1]]
+        idu += [exam_info[i][0]]
+        ids += [exam_info[i][1]]
         if (uid not in idu) and (ids not in sid):
             exam_info += [[uid, sid, [], [], 0]]
     return exam_info
@@ -21,7 +21,7 @@ def get_welcome_and_rules_msg():
     # จะมีการกล่าวต้อนรับอย่างไร และมีกฎกติกาอะไรต้องแจ้งบ้าง ออกทางหน้าจอ
 
     Rules = str("")
-    # print("พ่อมึงตาย ISUS มึงเข้ามาสอบทำไมวะ เขียนชื่อแล้วก็ออกไป คะแนนก็ได้เท่ากับตอนที่มึงสอบจริงนั่นแหละนะ")
+    # Rules += "พ่อมึงตาย ISUS มึงเข้ามาสอบทำไมวะ เขียนชื่อแล้วก็ออกไป คะแนนก็ได้เท่ากับตอนที่มึงสอบจริงนั่นแหละนะ"
     Rules += "สวัสดีครับนักศึกษาทุกคน วันนี้เป็นวันสอบวิชาการเขียนโปรแกรมนะครับ" + "\n"
     Rules += "มีเวลา 180 นาท" + "\n"
     Rules += "ข้อสอบมี 2 ส่วน คือ ปรนัย และเขียนโปรแกรมนะครับโปรดตรวจสอบให้เรียบร้อยก่อนทำ" + "\n"
@@ -39,20 +39,25 @@ def get_student_info(uid, exam_info):
     return None
 
 def get_question(uid, exam_info, all_questions):
-    idx = int(random.random() * len(all_questions))
+    # คืนผลเป็นคำถามจาก questions ที่ถูกชี้ด้วย qidx ถ้า ณ ตำแหน่งนั้นยังไม่มีคำถาม function นี้
+    # จะต้องไปสุ่มเลือกคำถามมาจาก all_questions ซึ่งจะต้องเป็นคำถามที่นิสิตยังไม่ได้ตอบ
+    # นอกจากจะคืนผลแล้ว function นี้จะต้องเพิ่มคำถามที่ได้ ต่อท้าย questions ด้วย
+    # แต่หากว่าคำถามหมดแล้วก็จะคืนคำว่า end มาให้โดยไม่เปลี่ยนแปลงใดๆกับ questions
+    idx = int(random.random() * len(all_questions) // 1)
     temp = get_student_info(uid, exam_info)
-    while all_questions[idx] == temp[2]:
+    while all_questions[idx] in temp[2]:
         if len(temp[2]) == len(all_questions):
             return "end"
         else:
-            idx = int(random.random() * len(all_questions))
-        temp[2] += [all_questions[idx]]
-        return all_questions[idx]
+            idx = int(random.random() * len(all_questions) // 1)
+    temp[2] += [all_questions[idx]]
+    return all_questions[idx]
 
 def submit_answer(uid, answer, exam_info):
-    info += get_student_info(uid, exam_info)
+    # แก้ไขค่าที่ qidx ชี้อยู่ที่ answers ให้มีค่าเป็น answer
+    # แต่ถ้าตำแหน่ง qidx ยังไม่มีค่าก็จะเอา answer ไปต่อท้าย ถัดจากนั้นก็จะเพิ่มค่า qidx ให้ชี้ไปยังคำถามถัดไป
+    info = get_student_info(uid, exam_info)
     if int(info[4]) == len(info[3]):
         info[3] += [answer]
     info[4] += 1
-    
-client.run(TOKEN)
+    return 
