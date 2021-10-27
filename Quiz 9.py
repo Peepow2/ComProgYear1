8c608bcbba81789a317ea20b385d5f3b67d5288a
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2021-10-27 13:21:02 +0700

    Delete Quiz 9.py

--- Quiz 9.py
+++
@@ -1,9 +0,0 @@
-def update(points, uses, reward):
-    for c in uses:
-        points[c] -= uses[c]
-    for c in points:
-        points[c] = int(points[c] * (100 + reward)/100)
-    return points
-
-exec(input().strip()) # ต้องมีบรรทัดนี้เมื่อส่งไป grader
-
