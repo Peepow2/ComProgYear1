a499057adc767cfa15dda6aded4ae0fc951ead8c
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2022-01-04 14:10:37 +0700

    Create longest common subsequence.py

---
+++ Homework (2-2564)/longest common subsequence.py
@@ -0,0 +1,19 @@
+def LCS(s1, s2):
+    l1 = len(s1) + 1; l2 = len(s2) + 1;
+    
+    count_LCS = [[0]*l2 for i in range(0, l1)]
+    
+    for i in range(1, l1):
+        for j in range(1, l2):
+            if s1[i-1] == s2[j-1]:
+                count_LCS[i][j] += count_LCS[i-1][j-1] + 1
+                
+            else:
+                count_LCS[i][j] = max(count_LCS[i-1][j], count_LCS[i][j-1])
+    # print(count_LCS)
+    return count_LCS[l1-1][l2-1]
+
+# --------------------------- Driver Code ---------------------------
+str1, str2 = input(), input()
+n = LCS(str1, str2)
+print(n)
