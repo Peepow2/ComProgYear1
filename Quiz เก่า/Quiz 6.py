be5b62eff9bae6c691f3510911b950955a1fb303
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2022-01-19 19:34:08 +0700

    Update Quiz 6.py

--- Quiz เก่า/Quiz 6.py
+++ Quiz เก่า/Quiz 6.py
@@ -5,6 +5,6 @@ if command == 'mean':
     print(sum(List_num)/len(List_num))  
 elif command == 'median':
     l = len(List_num)
-    print((List_num[(l-1)//2] + List_num[l//2])/2)
+    print((List_num[(l-1)//2] + List_num[l//2]) / 2)
 else:
     print("Not a command")
