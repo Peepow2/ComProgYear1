ddb3bfa46c0d057a333b34b8a2c5dc917a501a9d
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2022-01-19 19:33:38 +0700

    Create Quiz 6.py

---
+++ Quiz เก่า/Quiz 6.py
@@ -0,0 +1,10 @@
+List_num = [float(e) for e in input().split()]
+command = str(input().strip())
+
+if command == 'mean':
+    print(sum(List_num)/len(List_num))  
+elif command == 'median':
+    l = len(List_num)
+    print((List_num[(l-1)//2] + List_num[l//2])/2)
+else:
+    print("Not a command")
