125eac49236c268219ed84d041bc927e8e2f1174
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2022-01-21 00:55:50 +0700

    Create 01_Expr_08.py

---
+++ Data Types & Expressions/01_Expr_08.py
@@ -0,0 +1,13 @@
+def cube_root(y):
+    a = sqrt_n_times(y, 2)
+    b = a * sqrt_n_times(a, 2)
+    c = b * sqrt_n_times(b, 4)
+    d = c * sqrt_n_times(c, 8)
+    e = d * sqrt_n_times(d, 16)
+    return e * sqrt_n_times(e, 32)
+
+def main():
+    q = float(input())
+    print(cube_root(q))
+    
+exec(input()) # DON'T remove this line
