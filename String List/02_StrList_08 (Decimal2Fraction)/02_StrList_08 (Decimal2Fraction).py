66ede2a0417c23df74378e8769aea99366ed51a5
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2022-01-28 18:36:46 +0700

    Update 02_StrList_08 (Decimal2Fraction).py

--- String List/02_StrList_08 (Decimal2Fraction)/02_StrList_08 (Decimal2Fraction).py
+++ String List/02_StrList_08 (Decimal2Fraction)/02_StrList_08 (Decimal2Fraction).py
@@ -5,8 +5,7 @@
  # If you do it, your score will be cancel immediately.
 """
 import math as m
-str1 = input().strip().split(",")
-a, b, c = str1[0], str1[1], str1[2];
+a, b, c = input().strip().split(",")
 fac = int(a + b + c) - int(a + b);
 dev = 10**(len(b + c)) - 10**(len(b))
 print(fac // m.gcd(fac, dev), "/", dev // m.gcd(fac, dev))
