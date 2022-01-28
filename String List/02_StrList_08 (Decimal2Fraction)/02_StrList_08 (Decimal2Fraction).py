c6c242a17086b392bd91e24e5fcecce54f3a8e45
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2022-01-28 18:34:32 +0700

    Update 02_StrList_08 (Decimal2Fraction).py

--- String List/02_StrList_08 (Decimal2Fraction)/02_StrList_08 (Decimal2Fraction).py
+++ String List/02_StrList_08 (Decimal2Fraction)/02_StrList_08 (Decimal2Fraction).py
@@ -7,9 +7,6 @@
 import math as m
 str1 = input().strip().split(",")
 a, b, c = str1[0], str1[1], str1[2];
-leb = len(b); lec = len(c);
 fac = int(a + b + c) - int(a + b);
-dev = 10**(leb + lec) - 10**(leb)
-GCD = m.gcd(fac, dev)
-print(fac//GCD,"/",dev//GCD)
-
+dev = 10**(len(b + c)) - 10**(len(b))
+print(fac // m.gcd(fac, dev), "/", dev // m.gcd(fac, dev))
