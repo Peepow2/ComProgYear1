c05a9ba1da38b614a9657b8e6985435e9bb12bc0
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2022-02-03 08:23:25 +0700

    Update and rename Quiz 3 (2-2564).py to Quiz 3 (2_2564).py

--- Quiz เก่า/Quiz 3 (2_2564).py
+++ Quiz เก่า/Quiz 3 (2_2564).py
@@ -1,4 +1,5 @@
 months_of_year = "JAN FEB MAR APR MAY JUN JUL AUG SEP OCT NOV DEC".split()
 str1 = input().split("/"); str1[1] = int(str1[1]) - 1 
+
 dok = "*" * (19 + len(str1[0]))
 print(dok + '\n' + "* " + "DATE: " + str1[0] + "." + months_of_year[str1[1]] + "." + str1[2] + " *" + '\n' + dok)
