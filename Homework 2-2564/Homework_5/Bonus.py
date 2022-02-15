026932974eb41962d038f0687fd9a69962a59f90
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2022-02-15 23:28:12 +0700

    Create Bonus.py

---
+++ Homework 2-2564/Homework_5/Bonus.py
@@ -0,0 +1,14 @@
+def least_pn_having(x):
+    X = str(x); n = 1; 
+    while True:
+        posnums = position_numbers(n)
+        p = [1]
+        for i in range(1, n + 1):
+            p += [next_partition_number(p, posnums)]
+        cnt = 0; pos = 0; PN = str(p[n])
+        pos = PN.find(X[cnt])
+        while pos != -1: 
+            cnt += 1
+            if cnt < len(X):  pos = PN.find(X[cnt], pos + 1)
+            if cnt == len(X): return p[n]
+        n += 1
