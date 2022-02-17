17598ab07b2c6c0a892c5a94f24520308b6ee3f0
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2022-02-17 18:56:25 +0700

    Create Bonus (Fastest).py

---
+++ Homework 2-2564/Homework_5/Bonus (Fastest).py
@@ -0,0 +1,27 @@
+# --------------------------------------------------------- #
+def next_position_number(posnums):
+    L = len(posnums)
+    r = (L // 2) + 1; s = (L + 1) % 2 
+    return r * (3 * r + (-1) ** s) // 2
+# --------------------------------------------------------- #
+def next_partition_number(p, posnums):
+    m = len(p)
+    step = ans = idx = 0
+    while posnums[idx] <= m:
+        ans += (p[m-posnums[idx]] * (-1) ** int(step))
+        step += 0.5; idx += 1
+    return ans
+# --------------------------------------------------------- #
+def least_pn_having2(x):
+    x = str(x); L = len(x); 
+    posnums = [1]; p = [1];
+    while True:
+        posnums.append(next_position_number(posnums))
+        p.append(next_partition_number(p, posnums))
+        PN = str(p[-1]); cnt = 0
+        pos = PN.find(x[0], 0)
+        while pos != -1:
+            cnt += 1
+            if cnt == L: return p[-1]
+            pos = PN.find(x[cnt], pos + 1)
+# --------------------------------------------------------- #
