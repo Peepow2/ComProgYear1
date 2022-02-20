b5ae675c00c2a6716f31d0ceff4cfa0a0ac85562
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2022-02-20 22:05:23 +0700

    Update Solution.py

--- Homework 2-2564/Homework_6/Solution.py
+++ Homework 2-2564/Homework_6/Solution.py
@@ -21,3 +21,58 @@ def match_foods(nutrient, food_name):
         if temp[0] == mx: out += [temp[1]]
     return sorted(out)
 # ---------------------------------------------------------------- #
+def get_nutrient(nutrient, food_name):
+    L = match_foods(nutrient, food_name)
+    if L == []: return []
+
+    Nu = id2food(nutrient, L)
+    div = [0] * (len(nutrient[0]) - 2)
+    OUT = ['NA'] * (len(nutrient[0]) - 2)
+    for temp in Nu:
+        temp = temp[2::]
+        for i in range(len(temp)):
+            if temp[i] != 'NA':
+                if OUT[i] == 'NA': OUT[i] = 0
+                OUT[i] += temp[i]
+                div[i] += 1
+
+    for i in range(len(OUT)):
+        if OUT[i] != 'NA': OUT[i] /= div[i]
+    return OUT
+# ---------------------------------------------------------------- #
+def summarize_daily_intake(nutrient, intakes):
+    L = list(); Match = list(); day_false = [] 
+    for t in intakes:
+        idx = search_id(t[0], Match)
+        if idx == -1: Match += [[t[0]]]
+        M = match_foods(nutrient, t[1])
+        if M == []: day_false += [t[0]]
+        Match[idx] += id2food(nutrient, M)
+
+    for t in Match:
+        idx = search_id(t[0], L)
+        if idx == -1: L += [[t[0]] + ['NA'] * 6 + [True]]
+        for w in t[1::]:
+            w = w[2::]
+            for r in range(len(w)):
+                if w[r] != 'NA':
+                    if L[idx][r+1] == 'NA': L[idx][r+1] = 0
+                    L[idx][r+1] += float(w[r])
+    for d in day_false:
+        idx = search_id(d, L)
+        L[idx][-1] = False
+    return sorted(L)[::-1]
+# ---------------------------------------------------------------- #
+def id2food(nutrient, L):
+    Nu = list()
+    for i in nutrient:
+        for j in L:
+            if j == i[0]: Nu += [i];
+    return Nu
+# ---------------------------------------------------------------- #
+def search_id(s, L):
+    for i in range(len(L)):
+        if L[i][0] == s:
+            return i
+    return -1
+# ---------------------------------------------------------------- #
