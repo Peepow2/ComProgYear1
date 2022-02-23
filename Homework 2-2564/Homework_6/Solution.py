e34cebc34838ac51c08d0a315d74c9168b9b63c3
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2022-02-23 23:38:44 +0700

    Update Solution.py

--- Homework 2-2564/Homework_6/Solution.py
+++ Homework 2-2564/Homework_6/Solution.py
@@ -2,74 +2,77 @@
 def compute_similarity(food_name1, food_name2):
     F1 = food_name1.split()
     F2 = food_name2.split()
-    cnt = 0; s = len(F1) + len(F2)
-    for i in F1: 
-        for j in F2: 
-            if i == j: cnt += 1
-    return (cnt * 2) / s
+
+    count = 0; s = len(F1) + len(F2)
+    for s1 in F1:
+        for s2 in F2:
+            if s1 == s2: count += 1
+    return (count * 2) / s
 # ---------------------------------------------------------------- #
 def match_foods(nutrient, food_name):
-    OUT = list()
+    tod = list()
     for temp in nutrient:
         t = compute_similarity(temp[1], food_name)
-        if t > 0.5: OUT += [[t, temp[0]]]
-
-    if OUT == []: return []
-    mx = max(OUT)[0]
-    out = list()
-    for temp in OUT:
-        if temp[0] == mx: out += [temp[1]]
-    return sorted(out)
+        if t > 0.5: tod += [[t, temp[0]]]
+    if tod == []: return []
+    mx = max(tod)[0] # [0.857, id]
+    OUT = list()
+    for temp in tod:
+        if temp[0] == mx: OUT += [temp[1]]
+    return OUT
 # ---------------------------------------------------------------- #
-def get_nutrient(nutrient, food_name):
+def get_nutrient(nutrient,food_name):
     L = match_foods(nutrient, food_name)
     if L == []: return []
-
+    
     Nu = list()
     for i in nutrient:
         for j in L:
-            if j == i[0]: Nu += [i];
-
-    div = [0] * (len(nutrient[0]) - 2)
-    OUT = ['NA'] * (len(nutrient[0]) - 2)
+            if j == i[0]: Nu += [i]
+    
+    TIME = [0] * 6
+    SUM = ['NA'] * 6
+    
     for temp in Nu:
         temp = temp[2::]
         for i in range(len(temp)):
             if temp[i] != 'NA':
-                if OUT[i] == 'NA': OUT[i] = 0
-                OUT[i] += temp[i]
-                div[i] += 1
-
-    for i in range(len(OUT)):
-        if OUT[i] != 'NA': OUT[i] /= div[i]
-    return OUT
+                if SUM[i] == 'NA': SUM[i] = 0
+                SUM[i] += temp[i]
+                TIME[i] += 1
+    
+    for i in range(len(SUM)):
+        if SUM[i] != 'NA': SUM[i] /= TIME[i]
+    return SUM
 # ---------------------------------------------------------------- #
 def summarize_daily_intake(nutrient, intakes):
-    L = list(); Timeline = list(); day_false = []; Energy = list()
+    # [[date, food1, food2, ...]]
+    Timeline = list(); day_false = list()
     for t in intakes:
-        idx = search_id(t[0], Timeline)
-        if idx == -1: Timeline += [[t[0]]]
-        if match_foods(nutrient, t[1]) == []: day_false += [t[0]]
-        Timeline[idx] += [t[1]]
+        idx = find_date_index(t[0], Timeline)
+        if idx == -1: Timeline.append([t[0]])
+        if match_foods(nutrient, t[1]) == []: day_false.append(t[0])
+        Timeline[idx].append(t[1])
     
+    Energy = list()
     for t in Timeline:
-        idx = search_id(t[0], Energy)
-        if idx == -1: Energy += [[t[0]] + ['NA'] * 6 + [True]]
+        idx = find_date_index(t[0], Energy)
+        if idx == -1: Energy.append([t[0]] + ['NA'] * 6 + [True])
         for food_name in t[1::]:
-            g_nu = get_nutrient(nutrient, food_name)
+            g_nu = get_nutrient(nutrient,food_name)
             for x in range(len(g_nu)):
                 if g_nu[x] != 'NA':
                     if Energy[idx][x+1] == 'NA': Energy[idx][x+1] = 0
                     Energy[idx][x+1] += g_nu[x]
-                    
+    
     for d in day_false:
-        idx = search_id(d, Energy)
+        idx = find_date_index(d, Energy)
         Energy[idx][-1] = False
     return sorted(Energy)[::-1]
 # ---------------------------------------------------------------- #
-def search_id(s, L):
-    for i in range(len(L)):
-        if L[i][0] == s:
+def find_date_index(search, List):
+    for i in range(len(List)):
+        if List[i][0] == search:
             return i
     return -1
 # ---------------------------------------------------------------- #
