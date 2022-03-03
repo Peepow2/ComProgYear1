0171b1ad9b52b7880e635ffa17060fe434322649
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2022-03-03 19:08:55 +0700

    Update Quiz 6 (2_2564).py

--- Quiz เก่า/Quiz 6 (2_2564).py
+++ Quiz เก่า/Quiz 6 (2_2564).py
@@ -1,18 +1,13 @@
-def f1(d):
-    return sum(d)
+store = list()  
+inp = input().strip()
+while inp != 'q':
+    inp = inp.split()
+    store += [inp[0], float(inp[1])]
+    inp = input().strip()
 
-def f2():
-    List = list()
-    for i in range(int(input())): 
-      List.append(int(input()))
-    return List
-
-def f3(x, e):
-    List = list()
-    for i in x:
-        if i % e == 0: List.append(i)
-    return List
-
-def f4():
-    L = f2()
-    return f1(f3(L, 2)) - f1(f3(L, 3)) - f1(f3(L, 5))
+buys = input().strip().split()
+price = 0
+for b in buys:
+    idx = store.index(b)
+    price += store[idx + 1]
+print("total payment", price)
