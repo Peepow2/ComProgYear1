fc4c47c888701c2d8905a1171cecdf7ff90e3d92
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2022-03-21 21:38:18 +0700

    Create Solution (1 min).py

---
+++ Homework 2-2564/Homework_8/Solution (1 min).py
@@ -0,0 +1,78 @@
+# HW8_StrFile (ไม่ลบหรือแก้ไขบรรทัดนี้ หรือเพิ่มอะไรก่อนบรรทัดนี้ โดยเด็ดขาด)
+
+def read_csv(filename):
+    fin = open(filename, 'r')
+    data = fin.readlines()
+    fin.close()
+    return data
+# ------------------------------------------------------------------------------- #
+def get_unique_value(filename,column_name):
+    data = read_csv(filename)
+    if column_name in data[0].strip().split(','):
+      idx = data[0].strip().split(',').index(column_name)
+    else: return []
+
+    OUT = list()
+    for line in data[1:]:
+      line = line.strip().split(',')
+      if line[idx] not in OUT:
+        OUT.append(line[idx])
+    return OUT
+
+def count_announce_date_province(filename,date,province):
+    cnt = 0
+    for line in open(filename, 'r'):
+        line = line.strip().split(',')
+        if line[1] == date and line[7] == province: cnt += 1
+    return cnt  
+
+def count_value_in_column(filename,column_name,value):
+    cnt = 0
+    data = read_csv(filename)
+    if column_name in data[0].strip().split(','):
+      idx = data[0].strip().split(',').index(column_name)
+    else: return -1
+    
+    for line in data[1:]:
+       line = line.strip().split(',')
+       if line[idx] == value: cnt += 1       
+    return cnt
+
+def create_file_by_value_in_column(filename,fileout,column_name,value):
+    data = read_csv(filename)
+    if column_name in data[0].strip().split(','):
+      idx = data[0].strip().split(',').index(column_name)
+    else: return 
+
+    fout = open(fileout, 'w')
+    fout.write(data[0])
+    for line in data[1:]:
+        line = line.strip().split(',')
+        if line[idx] == value: 
+            fout.write(",".join(line) + '\n')
+    fout.close()
+    return
+
+def count_by_date(filename,province):
+    OUT = list()
+    data = read_csv(filename)
+    prev = "xx"
+
+    for line in data[1:]:
+        line = line.strip().split(',')
+        if line[1] != prev: 
+            OUT.append(0)
+            prev = line[1]
+        if line[7] == province: 
+            OUT[-1] += 1
+    return OUT
+
+def create_summary_file(filename,fileout):
+    date = get_unique_value(filename, 'announce_date')
+    province = sorted(get_unique_value(filename, 'province_of_isolation')) 
+    fout = open(fileout, 'w')
+    fout.write("province_of_isolation," + ",".join(date) + '\n')
+    for t in province:
+        fout.write(t + ',' + ",".join([str(e) for e in count_by_date(filename, t)]) + '\n')
+    fout.close()
+    return
