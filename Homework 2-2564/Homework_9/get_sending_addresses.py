2e4658f6fb1ba20740667a873caa2b084e411e9f
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2022-03-26 01:07:59 +0700

    Update get_sending_addresses.py

--- Homework 2-2564/Homework_9/get_sending_addresses.py
+++ Homework 2-2564/Homework_9/get_sending_addresses.py
@@ -1,9 +1,8 @@
-def get_sending_addresses(senders, receivers, address) :
+def get_sending_addresses(senders, receivers, address):
     out = list()
     for s in senders:
-        if s in receivers:
-            for r in receivers[s]:
-                out.append(address[r])
+        if receivers.get(s, 'empty') != 'empty':
+            out += [str(address[r]) for r in receivers[s]]
     return out
 
 print(get_sending_addresses(['พีพี', 'ยูนะ'], {'พีพี': ['อ๋อง', 'ไออุ่น', 'ยีน'], 'ยูนะ': ['อ๋อง', 'โบกี้', 'ปาย']}, {'พีพี' : '00/001' , 'ยูนะ' : '00/011', 'อ๋อง': '00/015', 'ไออุ่น': '00/025', 'ยีน': '00/005', 'โบกี้': '00/035', 'ปาย': '01/005'}))
