8d29c5442b1477e2fe89c8bebffc179b96d27fc3
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2022-03-26 00:56:52 +0700

    Update find_best_location.py

--- Homework 2-2564/Homework_9/find_best_location.py
+++ Homework 2-2564/Homework_9/find_best_location.py
@@ -4,5 +4,4 @@ def find_best_location(senders, receivers, address, discount , dispatch_centers)
         price[dc] = calculate_fee(senders, receivers, address, discount, dispatch_centers[dc])
     for dc in price:
         location[dc] = sum([int(price[dc][e]) for e in price[dc]])
-    for i in location: L.append([location[i], i])
-    return min(L)[::-1]
+    return min([[location[i], i] for i in location])[::-1]
