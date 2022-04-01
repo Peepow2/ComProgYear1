4173bb5ddd920a451b72591f40c1e9104f1b6e40
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2022-04-01 22:32:06 +0700

    Create Quiz 8 (2_2564).py

---
+++ Quiz เก่า/Quiz 8 (2_2564).py
@@ -0,0 +1,8 @@
+def get_secret(t):
+    t = t.split(t[0])[2::2]
+    return ''.join(t)
+  
+def print_secret(filename):
+    for line in open(filename, "r"):
+        print(get_secret(line))
+exec(input().strip()) # DON'T remove this line
