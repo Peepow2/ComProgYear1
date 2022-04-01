85718163e7d9038b2089fc5f6e6dab5998eec631
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2022-04-01 21:07:24 +0700

    Update Quiz 9 (2_2564).py

--- Quiz เก่า/Quiz 9 (2_2564).py
+++ Quiz เก่า/Quiz 9 (2_2564).py
@@ -9,8 +9,10 @@ def to_type2(type1):
     D = dict()
     for k in type1:
         k1, k2 = k.split(':')
-        D[k1] = D.get(k1, list()) + [k2]
-        D[k2] = D.get(k2, list()) + [k1]
+        D[k1] = D.get(k1, list())
+        D[k2] = D.get(k2, list())
+        if k2 not in D[k1]: D[k1].append(k2)
+        if k1 not in D[k2]: D[k2].append(k1)
     return D
 
 exec(input().strip()) # DON'T remove this line
