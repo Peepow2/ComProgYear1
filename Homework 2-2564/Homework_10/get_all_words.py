268753b4af8fe22e4677dc4dcdf60fe8cdfc80fb
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2022-04-02 20:57:46 +0700

    Create get_all_words.py

---
+++ Homework 2-2564/Homework_10/get_all_words.py
@@ -0,0 +1,22 @@
+def get_all_words(board):
+    words = [[], []]
+    check = list()
+    for i in range(15):
+        write = False
+        for j in range(15):
+            if write == False: check.append("")
+            write = (board[i][j] != '')
+            if write: check[-1] += board[i][j]
+            
+    for j in range(15):
+        write = False
+        for i in range(15):
+            if write == False: check.append("")
+            write = (board[i][j] != '')
+            if write: check[-1] += board[i][j]
+            
+    for c in sorted(check):
+        if len(c) > 1:
+            if is_valid(c): words[0].append(c)
+            else: words[1].append(c)
+    return words
