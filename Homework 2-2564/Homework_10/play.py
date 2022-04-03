4c4743ac3cb679a93c599d885e5cd4b08c074f8a
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2022-04-03 23:15:23 +0700

    Update play.py

--- Homework 2-2564/Homework_10/play.py
+++ Homework 2-2564/Homework_10/play.py
@@ -1,13 +1,11 @@
-def play(row,col,down,tiles,board):
     V, New_board = place_tiles(row, col, down, tiles, board)
-    if not V: return -1
     words1 = get_all_words(board)
-    words2 = get_all_words(New_board)
+    words2 = get_all_words(New_board)  
+    if not V or (len(words2[1]) > 0): return -1
     score = 0
     for w in words2[0]:
       if w not in words1[0]: 
         for c in w:
           score += get_value(c)
       else: words1[0].remove(w)
-    if len(tiles) >= 7: score += 50
-    return score
+    return score + (int(len(tiles) >= 7) * 50)
