012e717eae7b68392df7c0318a682aaf5158a6e6
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2022-04-03 22:09:32 +0700

    Update get_all_words.py

--- Homework 2-2564/Homework_10/get_all_words.py
+++ Homework 2-2564/Homework_10/get_all_words.py
@@ -2,7 +2,6 @@ def get_all_words(board):
     words = [[], []]
     W = ""
     for i in range(15):
-        W += ' '
         for j in range(15):
             if board[i][j] != '': W += board[i][j]
             else: W += ' '
@@ -10,6 +9,7 @@ def get_all_words(board):
         for j in range(15):
             if board[j][i] != '': W += board[j][i]
             else: W += ' '
+        W += ' '
     
     for c in sorted(W.split()):
         if len(c) > 1: words[(is_valid(c) + 1) % 2].append(c)
