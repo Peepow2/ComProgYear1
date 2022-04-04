bcef8b2da2297b896a47425101a55380fcd4b498
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2022-04-04 12:03:57 +0700

    Update get_play.py

--- Homework 2-2564/Homework_10/get_play.py
+++ Homework 2-2564/Homework_10/get_play.py
@@ -1,12 +1,11 @@
 def get_play(board, new_board):
-    x = y = d = 0
-    v = -1
     s = ''
+    pos = list()
     for i in range(15):
         for j in range(15):   
           if board[i][j] != new_board[i][j]: 
             s += new_board[i][j]
-            v = i; h = j;
-            if x == 0 and y == 0: x = j; y = i
-    if y < v and len(s) > 1: d = 1
-    return [y, x, d, s]
+            pos.append([j, i])
+    d = 0
+    if pos[0][1] < pos[-1][1]: d = 1
+    return [pos[0][1], pos[0][0], d, s]
