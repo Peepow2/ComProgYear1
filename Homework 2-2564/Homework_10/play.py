6ce10695ad8dd335551a2a3436b6b5848adcecb3
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2022-04-05 23:43:18 +0700

    Update play.py

--- Homework 2-2564/Homework_10/play.py
+++ Homework 2-2564/Homework_10/play.py
@@ -1,7 +1,7 @@
 def play(row,col,down,tiles,board):
     V, New_board = place_tiles(row, col, down, tiles, board)
-    words1 = get_all_words(board)
-    words2 = get_all_words(New_board)  
+    words1 = get_all_words_with_postion(board)
+    words2 = get_all_words_with_postion(New_board)  
     if not V or (len(words2[1]) > 0): return -1
     score = 0
     for w in words2[0]:
