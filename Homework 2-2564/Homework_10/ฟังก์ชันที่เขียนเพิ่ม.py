ea7853c54236fbd275917ffc53cb61691fad274f
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2022-04-06 17:58:12 +0700

    Update ฟังก์ชันที่เขียนเพิ่ม.py

--- Homework 2-2564/Homework_10/ฟังก์ชันที่เขียนเพิ่ม.py
+++ Homework 2-2564/Homework_10/ฟังก์ชันที่เขียนเพิ่ม.py
@@ -1,21 +1,23 @@
-def convert(i, j, W, pos, words, board):
-    if board[i][j] == '' or j == 14:
-        W = (W + board[i][j]).strip()
-        if len(W) > 1:
-            words[(is_valid(W) + 1) % 2].append([W, pos[0][0], pos[0][1], 1])
-        W = "" ; pos = []
-    else:
-        W += board[i][j]
-        pos.append([i, j])
-    return pos, W, words
-
 def get_all_words_with_postion(board):
     words = [[], []]
     pos = list()
     W = ""
     for i in range(15):
-        for j in range(15): 
-          pos, W, words = convert(i, j, W, pos, words, board)  
-        for j in range(15): 
-          pos, W, words = convert(j, i, W, pos, words, board)
+        for j in range(15):
+            if board[i][j] == '' or j == 14:
+                W = (W + board[i][j]).strip()
+                if len(W) > 1: words[(is_valid(W) + 1) % 2].append([W, pos[0][0], pos[0][1], 0])
+                W = "" ; pos = []
+            else:
+                W += board[i][j]
+                pos.append([i, j])
+    
+        for j in range(15):
+            if board[j][i] == '' or j == 14:
+                W = (W + board[j][i]).strip()
+                if len(W) > 1: words[(is_valid(W) + 1) % 2].append([W, pos[0][0], pos[0][1], 1])
+                W = "" ; pos = []
+            else:
+                W += board[j][i]
+                pos.append([j, i])
     return [sorted(words[0]), sorted(words[1])]
