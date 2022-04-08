6fdefb65fe5a04a8d9c030cdfc7c47093d70cde6
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2022-04-08 22:19:35 +0700

    Create merge_data.py

---
+++ Homework 2-2564/Homework_11/merge_data.py
@@ -0,0 +1,16 @@
+def merge_data(data1,data2):
+    OUT = dict()
+    for k1 in data1:
+        if k1 in data2:
+            OUT[k1] = data1[k1]
+            for k2 in data2[k1]:
+                if k2 not in OUT[k1]:
+                    OUT[k1][k2] = data2[k1][k2]
+                else:
+                    OUT[k1][k2 + '_1'] = data2[k1][k2]
+        else: OUT[k1] = data1[k1]
+            
+    for k2 in data2:
+        if k2 not in OUT:
+            OUT[k2] = data2[k2]
+    return OUT
