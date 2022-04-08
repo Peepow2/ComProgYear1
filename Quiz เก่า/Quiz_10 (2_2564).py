fbe8bb93f054866c2252f33cba6c648fe193a291
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2022-04-08 22:51:06 +0700

    Update Quiz_10 (2_2564).py

--- Quiz เก่า/Quiz_10 (2_2564).py
+++ Quiz เก่า/Quiz_10 (2_2564).py
@@ -1,4 +1,2 @@
-def m_sum(m, axis):
-    if axis == 0: m = zip(*m)
-    return [sum(e) for e in m]            
+def m_sum(m, axis): return [sum(e) for e in [zip(*m), m][axis]]           
 exec(input().strip()) #ต้องมีบรรทัดนี้เมื่อส่งไป grader
