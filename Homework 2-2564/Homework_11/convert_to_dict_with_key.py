aa9cc67c06196d9778ab1283da4bc6630f097a60
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2022-04-08 21:27:54 +0700

    Update convert_to_dict_with_key.py

--- Homework 2-2564/Homework_11/convert_to_dict_with_key.py
+++ Homework 2-2564/Homework_11/convert_to_dict_with_key.py
@@ -1,14 +1,10 @@
-def empty(D):      return D == {}
-def No_None(A, B): return A != None and B != None
-
 def convert_to_dict_with_key(data,column_name,key):
     OUT = dict()
     for i in range(len(data)):
         D = dict()
         for j in range(len(data[i])):
-            KEY, VAL = column_name[j], data[i][j]
-            if key == KEY: K = data[i][j]
+            if key == column_name[j]: K = data[i][j]
             else:
-                if No_None(KEY, VAL): D[KEY] = VAL         
-        if K != None and OUT.get(K) == None and not empty(D): OUT[K] = D
+                if data[i][j] != None: D[column_name[j]] = data[i][j]             
+        if (K != None) and (K not in OUT) and (D != {}): OUT[K] = D
     return OUT
