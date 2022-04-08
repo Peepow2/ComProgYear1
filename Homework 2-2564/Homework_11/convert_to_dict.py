3f03bad12e95527dffb151618d957d8a4f10f707
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2022-04-08 21:24:33 +0700

    Update convert_to_dict.py

--- Homework 2-2564/Homework_11/convert_to_dict.py
+++ Homework 2-2564/Homework_11/convert_to_dict.py
@@ -1,12 +1,8 @@
-def empty(D):      return D == {}
-def No_None(A, B): return A != None and B != None
-
 def convert_to_dict(data,column_name):
     out = list()
     for i in range(len(data)):
         D = dict()
         for j in range(len(data[i])):
-            k, v = column_name[j], data[i][j]
-            if No_None(k, v): D[k] = v
-        if not empty(D): out.append(D)
+            if data[i][j] != None : D[column_name[j]] = data[i][j]
+        if D != {}: out.append(D)
     return out
