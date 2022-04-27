8888c574a85a52538d132a7a095737a79c823f39
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2022-04-27 20:18:50 +0700

    Update animate_drop.py

--- Homework 2-2564/Homework_12/animate_drop.py
+++ Homework 2-2564/Homework_12/animate_drop.py
@@ -1,4 +1,8 @@
 import numpy as np
+
+def can_replace(board, shape): 
+    return np.sum(board != 0) + np.sum(shape != 0) == np.sum(new_board != 0)
+    
 def animate_drop(board, shape, c):
     W, L = shape.shape
     OUT = list()       
@@ -6,9 +10,9 @@ def animate_drop(board, shape, c):
         new_board = np.array(board)
         if i < 0:
             new_board[0:-i, c:c + L] += shape[i:]
-            if np.sum(board != 0) + np.sum(shape[i:] != 0) != np.sum(new_board != 0): return []   
+            if not can_replace(new_board, shape[i::]): return []   
         else:
             new_board[i:i + W, c:c + L] += shape
-            if np.sum(board != 0) + np.sum(shape != 0) != np.sum(new_board != 0): break
+            if not can_replace(new_board, shape): break
             OUT.append(new_board)
     return OUT
