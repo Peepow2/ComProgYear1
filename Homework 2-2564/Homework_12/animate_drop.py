e0828113bc0d2415e7d26b606ab6c075d4b052e9
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2022-04-27 16:30:42 +0700

    Update animate_drop.py

--- Homework 2-2564/Homework_12/animate_drop.py
+++ Homework 2-2564/Homework_12/animate_drop.py
@@ -2,11 +2,12 @@ import numpy as np
 def animate_drop(board, shape, c):
     W, L = shape.shape
     OUT = list()
+        
     for i in range(-len(shape), len(board) - W + 1):
         new_board = np.array(board)
         if i < 0:
             new_board[0:-i, c:c + L] += shape[i:]
-            if np.sum(board != 0) + np.sum(shape != 0) != np.sum(new_board != 0): return []
+            if np.sum(board != 0) + np.sum(shape[i:] != 0) != np.sum(new_board != 0): return []   
         else:
             new_board[i:i + W, c:c + L] += shape
             if np.sum(board != 0) + np.sum(shape != 0) != np.sum(new_board != 0): break
