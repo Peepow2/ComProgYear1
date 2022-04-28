cea0b9688d67e250654aa3ee82cfd34e59fb6022
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2022-04-28 08:26:23 +0700

    Create Quiz_12 (2_2564).py

---
+++ Quiz เก่า/Quiz_12 (2_2564).py
@@ -0,0 +1,13 @@
+import numpy as np
+def f1(v):
+    return np.sum(v != np.arange(len(v))) == 0
+
+def f2(u, v):
+    return np.array(u) + np.array(v[::-1])
+
+def f3(M, v):
+    return M * v
+
+#----- DON'T modify any of the following lines -----
+for k in range(int(input())):
+    exec(input().strip())
