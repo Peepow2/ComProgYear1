4e4f661856f826b729b107b3c425b6ec5ba6605f
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2022-04-29 22:59:19 +0700

    Update Solution.py

--- Homework 2-2564/Homework_12/Solution.py
+++ Homework 2-2564/Homework_12/Solution.py
@@ -7,54 +7,53 @@
 import numpy as np
 
 def get_score(board):
-    return np.sum(np.sum(board == 0, axis = 1) == 0) * 40
+    M = np.min(board, axis = 1)
+    return np.sum(M != 0) * 40
 
 def get_time_cap(board, time_caps):
-    pos = np.arange(1, len(board) + 1)
-    n = pos[np.max(board, axis = 1) != 0]
-    if len(n) == 0: n = len(board)
-    else: n = int(n[0])
-    return time_caps[ceil(n, (len(board) // len(time_caps))) - 1]
+    M = np.max(board, axis = 1)
+    pos = np.arange(len(board))
+    n = pos[M != 0]
+    if len(n) == 0: return time_caps[-1]
+    n = int(n[0])
+    idx = ceil(n+1, len(board) // len(time_caps)) - 1 
+    return time_caps[idx]
 
 def rotate_right(shape):
     return np.array(shape.T[::, ::-1])
 
 def rotate_left(shape):
     return np.array(shape.T[::-1, ::])
-    
+
 def animate_drop(board, shape, c):
+    Zero = np.sum(board != 0) + np.sum(shape != 0)
     W, L = shape.shape
-    OUT = list()       
-    for i in range(-len(shape), len(board) - W + 1):
-        new_board = np.array(board)
+    OUT = list()
+    for i in range(-shape.shape[0], len(board) - W + 1):
+        new_board = np.array(board) 
         if i < 0:
-            new_board[0:-i, c:c + L] += shape[i:]
-            if not can_replace(new_board, shape[i:]): return []   
+          new_board[0:-i, c:c+L] += shape[i:]
+          if np.sum(new_board != 0) != np.sum(board != 0) + np.sum(shape[i:] != 0): return []
         else:
-            new_board[i:i + W, c:c + L] += shape
-            if not can_replace(new_board, shape): break
-            OUT.append(np.array(new_board))
+          new_board[i:i+W, c:c+L] += shape
+          if np.sum(new_board != 0) != Zero: break
+          OUT.append(np.array(new_board))
     return OUT
 
 def animate_clear(board):
     new_board = np.array(board)
-    new_board[np.min(board, axis = 1) != 0, :] = 0
-    nr = np.sum(np.sum(new_board, axis = 1) != 0) 
-    pos = np.arange(len(board))
+    row = np.min(board, axis = 1) != 0
+    if np.sum(row) == 0: return []
+    new_board[row, :] = 0
     
-    OUT = list()
-    OUT.append(np.array(new_board))   
-    while True:
-        idx = np.sum(new_board, axis = 1) == 0
-        if np.sum(idx) == 0: return []
-        
-        c = pos[idx][-1]
-        if c != len(board):
-            new_board[1:c+1] = new_board[0:c]
+    OUT = [np.array(new_board)]
+    R = board.shape[0]
+    while R > 1 and np.sum(new_board[0:R] != 0) > 0:
+        if np.sum(new_board[R-1]) == 0:
+            new_board[1:R] = new_board[0:R-1]
             new_board[0] = 0
-            OUT.append(np.array(new_board))       
-        if np.sum(np.sum(new_board[-nr:], axis = 1) != 0) == nr: break
+            OUT.append(np.array(new_board))
+        else: R -= 1
     return OUT
 
-def ceil(a, b): return -(-a // b)
-def can_replace(board, shape): return np.sum(board != 0) + np.sum(shape != 0) == np.sum(new_board != 0)
+def ceil(a, b): return -(-a//b)
