fbfa639a9570fd75df8ab6ffe082730f034152c2
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2022-04-30 12:56:39 +0700

    Update Solution.py

--- Homework 2-2564/Homework_12/Solution.py
+++ Homework 2-2564/Homework_12/Solution.py
@@ -7,17 +7,14 @@
 import numpy as np
 
 def get_score(board):
-    M = np.min(board, axis = 1)
-    return np.sum(M != 0) * 40
+    return np.sum(np.min(board, axis = 1) != 0) * 40
 
 def get_time_cap(board, time_caps):
     M = np.max(board, axis = 1)
-    pos = np.arange(len(board))
-    n = pos[M != 0]
+    Index = np.arange(len(board))
+    n = Index[M != 0]
     if len(n) == 0: return time_caps[-1]
-    n = int(n[0])
-    idx = ceil(n+1, len(board) // len(time_caps)) - 1 
-    return time_caps[idx]
+    return time_caps[ceil(int(n[0]) + 1, len(board) // len(time_caps)) - 1]
 
 def rotate_right(shape):
     return np.array(shape.T[::, ::-1])
@@ -42,18 +39,21 @@ def animate_drop(board, shape, c):
 
 def animate_clear(board):
     new_board = np.array(board)
-    row = np.min(board, axis = 1) != 0
-    if np.sum(row) == 0: return []
-    new_board[row, :] = 0
+    new_board[np.min(board, axis = 1) != 0, :] = 0
+    nr = np.sum(np.sum(new_board, axis = 1) != 0) 
+    pos = np.arange(len(board))
     
-    OUT = [np.array(new_board)]
-    R = board.shape[0]
-    while R > 1 and np.sum(new_board[0:R] != 0) > 0:
-        if np.sum(new_board[R-1]) == 0:
-            new_board[1:R] = new_board[0:R-1]
+    OUT = [np.array(new_board)]   
+    while True:
+        idx = np.sum(new_board, axis = 1) == 0
+        if np.sum(idx) == 0 or np.sum(board) == 0: return []
+        
+        c = pos[idx][-1]
+        if np.sum(np.sum(new_board[-nr:], axis = 1) != 0) == nr: break   
+        if c < len(board):
+            new_board[1:c+1] = new_board[0:c]
             new_board[0] = 0
-            OUT.append(np.array(new_board))
-        else: R -= 1
+            OUT.append(np.array(new_board))     
     return OUT
 
 def ceil(a, b): return -(-a//b)
