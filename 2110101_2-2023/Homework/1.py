d5ecf90604d1d0ad0fd308b18a90c5426725d20a
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2023-02-07 20:41:02 +0700

    Update 1.py

--- 2110101_2-2023/Homework/1.py
+++ 2110101_2-2023/Homework/1.py
@@ -6,7 +6,7 @@ def distance(r1, r2):
     return ((c2[0] - c1[0]) ** 2 + (c2[1] - c1[1]) ** 2) ** 0.5
 
 def intersection(r1, r2):
-    return max(0.0, min(r1[0] + r1[2], r2[0] + r2[2]) - max(r1[0], r2[0])) * max(0.0, min(r1[1], r2[1]) - max(r1[1] - r1[3], r2[1] - r2[3]))
+    return max(0, min(r1[0] + r1[2], r2[0] + r2[2]) - max(r1[0], r2[0])) * max(0, min(r1[1], r2[1]) - max(r1[1] - r1[3], r2[1] - r2[3]))
 
 def union(r1, r2):
     return (r1[2] * r1[3]) + (r2[2] * r2[3]) - intersection(r1, r2)
