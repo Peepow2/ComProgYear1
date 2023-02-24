fa3ac3cf4053c9d045b66eacc9e66fa0c939001e
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2023-02-24 21:42:39 +0700

    Update QUIZ1.py

--- 2110101_2-2023/QUIZ/QUIZ1.py
+++ 2110101_2-2023/QUIZ/QUIZ1.py
@@ -42,3 +42,23 @@ for i in range(1, 8):
 print("-------------")
 
 # QUIZ1_3 Flowchart โจทย์ยาวจำไม่ได้
+a,b,c,d = [int(e) for e in input().split()]
+if d > a:
+    n = 0
+    while a < d:
+        n += 1
+        a += abs(b)
+        d -= abs(c)
+    print(n ,a, d)
+else:
+    a, d = d, a
+    if a > b:
+        a, b = b, a
+    if c > d:
+        c, d = d, c
+    if b == c:
+        print(a, b, d)
+    elif b > c:
+        print((b+c)/2)
+    else:
+        print(a,b,c,d)
