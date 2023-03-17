425536cff97717ce27b7e9600f0ba9ae2d955350
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2023-03-17 23:53:03 +0700

    Update 2565_1_Sample_Quiz_2.py

--- 2110101_2-2023/QUIZ/2565_1_Sample_Quiz_2.py
+++ 2110101_2-2023/QUIZ/2565_1_Sample_Quiz_2.py
@@ -1,3 +1,4 @@
+# Version 1
 def check_string(T, S):
     T , S = T.upper(), S.upper()
     for i in range(len(S)):
@@ -21,4 +22,29 @@ for line in f.readlines():
             str1 += line[i]
             i += 1
     str1 += line[i:]
+print(str1)
+# -------------------------------------------------- #
+# Version 2
+def match(T, S):
+    T, S = T.upper(), S.upper()
+    for i in range(len(S)):
+        if (T[i] != S[i] and T[i] != '?') or len(T) != len(S):
+            return False
+    return True
+
+# Driver Code (main)
+FILENAME = input().strip()
+O_NAME = input().strip() 
+N_NAME = input().strip()
+Data = open(FILENAME, 'r').read()
+str1 = ''
+k1 = 0
+while True:
+    k2 = Data.find('/', k1 + 1)
+    if k2 == -1: break
+    if match(O_NAME, Data[k1:k2]):
+        str1 += N_NAME + '/'
+    else:
+        str1 += Data[k1:k2] + '/'
+    k1 = k2 + 1
 print(str1)
